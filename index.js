91a62ed17f5087e3b9cfb477ab6c18e5769268ff
Nina <your_email@example.comNina>
2024-10-15 15:51:44 -0400

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log (`test`)
+// I need to create a prompt  to enter a list of comma-separated froyo flavors. 
+// They type vanilla,vanilla,vanilla,strawberry,coffee,coffee
+
+// In my prompt, I need to separate each word by a comma using .split 
