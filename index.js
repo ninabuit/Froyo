a1d93fd0354ae252d655b39f2f07024f0394be6b
ninabuit <ninabuit93@gmail.com>
2024-10-15 20:23:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,18 @@
 // They type vanilla,vanilla,vanilla,strawberry,coffee,coffee
 
 // In my prompt, I need to separate each word by a comma using .split 
+
+const froyoOrder = prompt(`please enter in flavors separated by a comma`, `vanilla,vanilla,vanilla,strawberry,chocolate`);
+const customerOrder = froyoOrder.split(",");
+
+const theOrders = {};
+// customerORder = [vanilla,vanilla,vanilla,strawberry,chocolate] 
+for (let i = 0; i < customerOrder.length; i++){
+    const ordersReceived = customerOrder[i]
+    if (theOrders[ordersReceived]) {
+        theOrders [ordersReceived]++
+
+    }
+    else theOrders[ordersReceived] = 1;
+}
+console.log(theOrders);
